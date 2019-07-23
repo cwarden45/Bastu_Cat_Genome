81aa90d84edc24f61421f009ed4df3f6f10d78bd
Charles Warden <cwarden45@gmail.com>
2019-07-22 20:49:14 -0700

    Add files via upload

--- Basepaws_Notes/ADMIXTURE+RFMix_ReAnalysis_with_Gandolfi-SNP-chip/combine_VCF.pl
+++ Basepaws_Notes/ADMIXTURE+RFMix_ReAnalysis_with_Gandolfi-SNP-chip/combine_VCF.pl
@@ -7,11 +7,11 @@ use diagnostics;
 my $individual_ID = "Bastu";
 my $individual_gender = 0;#female
 my $sample_name = "basepaws";
-my $VCF_Individual = "HCWGS0003.g.vcf";
-my $VCF_prev = "Gandolfi_felCat9.vcf";
-my $VCF_Combined = "Gandolfi_felCat9_plus_Bastu.vcf";
-my $prev_ped = "Gandolfi_felCat9.ped";
-my $updated_ped = "Gandolfi_felCat9_plus_Bastu.ped";
+my $VCF_Individual = "../felCat8.gatk.flagged.gVCF";
+my $VCF_prev = "Gandolfi_felCat8.vcf";
+my $VCF_Combined = "Gandolfi_felCat8_plus_Bastu.vcf";
+my $prev_ped = "Gandolfi_felCat8.ped";
+my $updated_ped = "Gandolfi_felCat8_plus_Bastu.ped";
 my $GATK4_flag = 1;
 my $large_flag = 1;
 
@@ -77,6 +77,27 @@ if($large_flag == 1){
 
 print "Reading individual VCF...\n";
 
+my %chr_map_hash;
+$chr_map_hash{"chrA1"}=1;
+$chr_map_hash{"chrA2"}=2;
+$chr_map_hash{"chrA3"}=3;
+$chr_map_hash{"chrB1"}=4;
+$chr_map_hash{"chrB2"}=5;
+$chr_map_hash{"chrB3"}=6;
+$chr_map_hash{"chrB4"}=7;
+$chr_map_hash{"chrC1"}=8;
+$chr_map_hash{"chrC2"}=9;
+$chr_map_hash{"chrD1"}=10;
+$chr_map_hash{"chrD2"}=11;
+$chr_map_hash{"chrD3"}=12;
+$chr_map_hash{"chrD4"}=13;
+$chr_map_hash{"chrE1"}=14;
+$chr_map_hash{"chrE2"}=15;
+$chr_map_hash{"chrE3"}=16;
+$chr_map_hash{"chrF1"}=17;
+$chr_map_hash{"chrF2"}=18;
+$chr_map_hash{"chrX"}=19;
+
 $line_count=0;
 open(INPUTFILE, $VCF_Individual) || die("Could not open $VCF_Individual!");
 while (<INPUTFILE>){
@@ -100,37 +121,41 @@ while (<INPUTFILE>){
 		my $format = $line_info[8];
 		my $geno = $line_info[9];
 		
-		if($GATK4_flag == 1){
-		
-			if($alt eq "<NON_REF>"){
-				$alt = $ref;
-			}else{
-				$alt =~ s/,<NON_REF>//;
-			}
+		if(exists($chr_map_hash{$chr})){
+			$chr=$chr_map_hash{$chr};
 			
-			$geno = substr($line_info[9],0,3);
+			if($GATK4_flag == 1){
 			
-			if(($geno eq "")|($geno eq "   ")){
-				$geno="./.";
-				print "Corrected empty geno value\n";
-			}elsif(!$geno =~ /\//){
-				print "Wrong formatting for |$geno|\n";
-				exit;
-			}
-		}#end if(($GATK4_flag == 1)&($alt eq "<NON_REF>"))
+				if($alt eq "<NON_REF>"){
+					$alt = $ref;
+				}else{
+					$alt =~ s/,<NON_REF>//;
+				}
+				
+				$geno = substr($line_info[9],0,3);
+				
+				if(($geno eq "")|($geno eq "   ")){
+					$geno="./.";
+					print "Corrected empty geno value\n";
+				}elsif(!$geno =~ /\//){
+					print "Wrong formatting for |$geno|\n";
+					exit;
+				}
+			}#end if(($GATK4_flag == 1)&($alt eq "<NON_REF>"))
 
-		my $varID = "$chr:$pos:$ref:$alt";
-		#print "$varID\n";
-		
-		if($large_flag == 1){
-			unless(exists($individual_hash{$varID})){
-				$filter = "FAIL";
-			}#end unless(exists($individual_hash{$varID}))
-		}#end if($large_flag == 1)
-		
-		if($filter eq "PASS"){
-			$individual_hash{$varID}=$geno;
-		}#end if($filter eq "PASS")
+			my $varID = "$chr:$pos:$ref:$alt";
+			#print "$varID\n";
+			
+			if($large_flag == 1){
+				unless(exists($individual_hash{$varID})){
+					$filter = "FAIL";
+				}#end unless(exists($individual_hash{$varID}))
+			}#end if($large_flag == 1)
+			
+			if($filter eq "PASS"){
+				$individual_hash{$varID}=$geno;
+			}#end if($filter eq "PASS")
+		}#end if(exists($chr_map_hash{$chr}))
 	}#end if(!($line =~ /^#/))
 }#end while (<INPUTFILE>)
 			
