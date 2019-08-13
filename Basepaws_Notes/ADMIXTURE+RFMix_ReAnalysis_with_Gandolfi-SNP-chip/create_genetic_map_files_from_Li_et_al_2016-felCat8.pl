565592275ec41609b6e92f8bfa3ea1057d60a3a5
Charles Warden <cwarden45@gmail.com>
2019-08-12 19:57:05 -0700

    Add files via upload

--- Basepaws_Notes/ADMIXTURE+RFMix_ReAnalysis_with_Gandolfi-SNP-chip/create_genetic_map_files_from_Li_et_al_2016-felCat8.pl
+++ Basepaws_Notes/ADMIXTURE+RFMix_ReAnalysis_with_Gandolfi-SNP-chip/create_genetic_map_files_from_Li_et_al_2016-felCat8.pl
@@ -2,7 +2,10 @@ use warnings;
 use strict;
 use Text::CSV;
 
+#NOTE: Some of the positions in the Gandolfi et al. 2018 table are off by 1 (relative to this table)
+
 my $Gandolfi_supplemental_table = "Supplementary_data_file_2_CatArrayMap.map";
+my $GangLi_PostPublication_Mapping_file = "probe_match_GangLi_GitHub.txt";
 my $output_folder = "felCat8";
 
 #I've converted the Excel file from the Li et al. 2016 Supplemental Table S1 into separate .csv files (per-chromosome, without "chr" in the name);
@@ -31,13 +34,40 @@ my %output_hash;
 
 foreach my $output_chr (keys %chr_map_hash){
 	my $map_chr = $chr_map_hash{$output_chr};
-	my $chr_out = "$output_folder/genetic_map_$output_chr\_felCat8-from-felCat6.txt";
+	my $chr_out = "$output_folder/genetic_map_$output_chr\_felCat8-probe-mapped.txt";
 	open($output_hash{$map_chr},"> $chr_out") || die("Could not open $chr_out!");
-	print {$output_hash{$map_chr}} "position COMBINED_rate(cM/Mb) Genetic_Map(cM)";
+	print {$output_hash{$map_chr}} "position COMBINED_rate(cM/Mb) Genetic_Map(cM)\n";
 }#end foreach my $VCF_chr (@chr_short)
 
-#define cM hash (from felCat68)
-print "Define Li et al. 2016...\n";
+#Gandolfi uses 2X probe names (I believe for felCat2?)
+print "Define hash to convert probes\n";
+
+my %probe_hash;
+
+my $line_count=0;
+open(INPUT, $GangLi_PostPublication_Mapping_file) || die("Could not open $GangLi_PostPublication_Mapping_file!");
+while (<INPUT>){
+		my $line = $_;
+		chomp $line;
+		$line =~ s/\r//g;
+		$line =~ s/\n//g;
+		$line_count++;
+		if($line_count > 1){
+			my @line_info = split("\t", $line);
+			my $probe_2x = $line_info[0];
+			my $probe_felCat6 = $line_info[1];
+			my $probe_felCat8 = $line_info[2];
+			if($probe_felCat8 =~ /^(chr\w\d)/){
+				my ($chr_felCat8) = ($probe_felCat8 =~ /^(chr\w\d)/);
+				if(($probe_2x =~ /^$chr_felCat8/)&($probe_felCat6 =~ /^$chr_felCat8/)){
+					$probe_hash{$probe_felCat6}=$probe_2x;
+				}#end if(($probe_2x =~ /^$chr_felCat8/)&($probe_felCat6 =~ /^$chr_felCat8/))
+			}#end if($probe_felCat8 =~ /^(chr\w\d)/)
+		}#end if($line_count > 1)
+}#end while (<INPUT>)
+close(INPUT);
+
+print "Define Li et al. 2016 mapping hash...\n";
 my %cM_hash;
 
 foreach my $chr (keys %chr_map_hash){
@@ -50,7 +80,7 @@ foreach my $chr (keys %chr_map_hash){
 	
 	my $CSV_input = "$CSV_chr.csv";
 
-	my $line_count=0;
+	$line_count=0;
 	open(INPUTCSV, $CSV_input) || die("Could not open $CSV_input!");
 	while (<INPUTCSV>){
 		my $line = $_;
@@ -66,7 +96,13 @@ foreach my $chr (keys %chr_map_hash){
 			if (($cM ne "")&($cM ne " ")){
 				for (my $i=0; $i < scalar(@fields); $i++){
 					if (($fields[$i] ne "")&($fields[$i]ne " ")){
-						$cM_hash{$fields[$i]}=$cM;
+						if(exists($probe_hash{$fields[$i]})){
+							my $mappable_ID = $probe_hash{$fields[$i]};
+							$cM_hash{$mappable_ID}=$cM;
+						}else{
+							#print "Skipping probe ID: $fields[$i]\n";
+							#exit;
+						}
 						last;
 					}#end if (($fields[$i] ne "")&($fields[$i]ne " "))
 				}#end for (my $i=0; $i < scalar(@fields); $i++)
@@ -84,9 +120,13 @@ print "Print probes in Gandolfi et al. 2018 felCat8 position order...\n";
 
 #I used this to figure out the 2nd column in the genetic map file format: https://www.biostars.org/p/222697/
 
-my $prev_chr = "";
-my $prev_pos_Mb = 0;
+my $prev_chr = 0;
+
+my $prev_pos = 0;
 my $prev_cM = 0;
+
+my $prev_max_pos = 0;
+my $prev_max_cM = 0;
 
 open(INPUT, $Gandolfi_supplemental_table) || die("Could not open $Gandolfi_supplemental_table!");
 while (<INPUT>){
@@ -99,11 +139,40 @@ while (<INPUT>){
 		my $probeID = $line_info[1];
 		my $felCat8_pos = $line_info[3];
 		
-		print "|$probeID|\n";
-		
 		if(exists($output_hash{$felCat8_chr})&(exists($cM_hash{$probeID}))){
-			print $line,"\n";
-			exit;
+			my $pos_Mb = $felCat8_pos / 1000000;
+			my $cM = $cM_hash{$probeID};
+			
+			if($felCat8_chr == $prev_chr){
+				#next position in same chr
+			}else{
+				#hopefully, start new chr.  Otherwise, I will have other issues.
+				print "Starting genetic mapping file output for chr $felCat8_chr...\n";
+				
+				$prev_pos = 0;
+				$prev_cM = 0;
+
+				$prev_max_pos = 0;
+				$prev_max_cM = 0;
+
+				$prev_chr=$felCat8_chr;
+			}#end else
+			
+			if(($cM > $prev_max_cM)&($pos_Mb > $prev_max_pos)){
+				#I used this to figure out the 2nd column in the genetic map file format: https://www.biostars.org/p/222697/
+				my $combined_rate = ($cM-$prev_cM)/($pos_Mb-$prev_pos);
+							
+				print {$output_hash{$felCat8_chr}} "$felCat8_pos $combined_rate $cM\n";
+							
+				$prev_pos = $pos_Mb;
+				$prev_cM = $cM;
+				if($prev_pos > $prev_max_pos){
+					$prev_max_pos=$prev_pos;
+				}
+				if($prev_cM > $prev_max_cM){
+					$prev_max_cM=$prev_cM;
+				}
+			}
 		}#end if(exists($output_hash{$felCat8_chr})&(exists($cM_hash{$probeID})))
 	}#end while (<INPUT>)
 close(INPUT);
