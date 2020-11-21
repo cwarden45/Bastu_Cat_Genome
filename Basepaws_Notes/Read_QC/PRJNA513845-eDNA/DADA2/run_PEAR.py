bc1dd443ff008dec4c4b7fbc5e5aad8ce23512f7
Charles Warden <cwarden45@gmail.com>
2020-11-21 15:09:24 -0800

    Add files via upload

---
+++ Basepaws_Notes/Read_QC/PRJNA513845-eDNA/DADA2/run_PEAR.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import re
+
+finishedSamples = []
+inputFolder = "DADA2_Processed_Reads"
+outputFolder = "PEAR"
+
+fileResults = os.listdir(inputFolder)
+
+for file in fileResults:
+	result = re.search("(.*)_1.fastq.gz$",file)
+	
+	if result:
+		sample = result.group(1)
+		if sample not in finishedSamples:
+			print(sample)
+
+			#run paired-end cutadapt
+			read1 = inputFolder + "/" + file
+			read2 = re.sub("_1.fastq","_2.fastq",read1)
+
+			pearPrefix = outputFolder + "/" + sample
+
+			#with multi-tasking, run with only 1 thread
+			command = "/opt/pear-0.9.11-linux-x86_64/bin/pear -f " + read1 + " -r " + read2 + " -o " + pearPrefix + " -j 1"
+			os.system(command)
