d21470ff20ab4d494d3de6c4a497e845c8c46749
Charles Warden <cwarden45@gmail.com>
2020-12-06 16:45:32 -0800

    Add files via upload

--- Basepaws_Notes/Read_QC/PRJNA513845-eDNA/OTU_clustering/run_vsearch.py
+++ Basepaws_Notes/Read_QC/PRJNA513845-eDNA/OTU_clustering/run_vsearch.py
@@ -13,11 +13,11 @@ command = "/opt/vsearch-2.15.1/bin/vsearch --cluster_fast "+inputFA+" --id 0.97 
 #--cluster_smallmem --usersort
 OTUfa = "FLASH_combined_VSEARCH_cluster_smallMEM.fa"
 OTUinfo = "FLASH_combined_VSEARCH_cluster_smallMEM.txt"
-command = "/opt/vsearch-2.15.1/bin/vsearch --cluster_smallmem --usersort "+inputFA+" --id 0.97 --centroids "+OTUfa+" --uc "+OTUinfo
+command = "/opt/vsearch-2.15.1/bin/vsearch --cluster_smallmem --usersort "+inputFA+" --id 0.98 --centroids "+OTUfa+" --uc "+OTUinfo
 #os.system(command)
 
 #--cluster_unoise
 OTUfa = "FLASH_combined_VSEARCH_unoise.fa"
 OTUinfo = "FLASH_combined_VSEARCH_unoise.txt"
-command = "/opt/vsearch-2.15.1/bin/vsearch --cluster_unoisef "+inputFA+" --id 0.97 --centroids "+OTUfa+" --uc "+OTUinfo
+command = "/opt/vsearch-2.15.1/bin/vsearch --cluster_unoise "+inputFA+" --id 0.98 --centroids "+OTUfa+" --uc "+OTUinfo
 os.system(command)
