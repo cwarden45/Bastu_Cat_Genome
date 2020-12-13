b7110c0936d5c21c0aceb15274ef85abcc1f7832
Charles Warden <cwarden45@gmail.com>
2020-12-12 16:07:26 -0800

    Add files via upload

--- Basepaws_Notes/Read_QC/PRJNA513845-eDNA/OTU_clustering/run_vsearch.py
+++ Basepaws_Notes/Read_QC/PRJNA513845-eDNA/OTU_clustering/run_vsearch.py
@@ -2,22 +2,14 @@ import os
 import sys
 import re
 
-inputFA = "FLASH_combined_unique_seqs.fa"
+inputFA = "FLASH_combined_unique_seqs-min_2_reads.fa"
 
 #--cluster_fast
-OTUfa = "FLASH_combined_VSEARCH_cluster.fa"
-OTUinfo = "FLASH_combined_VSEARCH_cluster.txt"
+OTUfa = "FLASH_combined_VSEARCH_cluster-min_2_reads.fa"
+OTUinfo = "FLASH_combined_VSEARCH_cluster-min_2_reads.txt"
 command = "/opt/vsearch-2.15.1/bin/vsearch --cluster_fast "+inputFA+" --id 0.97 --centroids "+OTUfa+" --uc "+OTUinfo
-#os.system(command)
-
-#--cluster_smallmem --usersort
-OTUfa = "FLASH_combined_VSEARCH_cluster_smallMEM.fa"
-OTUinfo = "FLASH_combined_VSEARCH_cluster_smallMEM.txt"
-command = "/opt/vsearch-2.15.1/bin/vsearch --cluster_smallmem --usersort "+inputFA+" --id 0.98 --centroids "+OTUfa+" --uc "+OTUinfo
-#os.system(command)
-
-#--cluster_unoise
-OTUfa = "FLASH_combined_VSEARCH_unoise.fa"
-OTUinfo = "FLASH_combined_VSEARCH_unoise.txt"
-command = "/opt/vsearch-2.15.1/bin/vsearch --cluster_unoise "+inputFA+" --id 0.98 --centroids "+OTUfa+" --uc "+OTUinfo
 os.system(command)
+
+#I think --cluster_smallmem --usersort could also work, but even the largest file doesn't crash (it just takes too long)
+
+#I think --cluster_unoise is better when abundance can be used to give weights to certain sequences
