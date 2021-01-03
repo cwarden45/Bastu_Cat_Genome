ffd5b6ea68480cab536390ae724da70886d75041
Charles Warden <cwarden45@gmail.com>
2021-01-03 15:02:43 -0800

    Add files via upload

--- Basepaws_Notes/Read_QC/PRJNA513845-eDNA/metagenomics/run_megablast-FASTA.sh
+++ Basepaws_Notes/Read_QC/PRJNA513845-eDNA/metagenomics/run_megablast-FASTA.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-INFA=../OTU_clustering/FLASH_combined_unique_seqs-min_2_reads-Swarm_OTU_with_counts.fa
-OUTHITS=../OTU_clustering/FLASH_combined_unique_seqs-min_2_reads-Swarm_OTU_with_counts.megablast
+
+#INFA=../OTU_clustering/FLASH_combined_unique_seqs-min_2_reads-Swarm_OTU_with_counts.fa
+#OUTHITS=../OTU_clustering/FLASH_combined_unique_seqs-min_2_reads-Swarm_OTU_with_counts.megablast
+
+INFA=../OTU_clustering/FLASH_combined_unique_seqs-min_2_reads--FILTER_OTU_MIN_100_READS-Swarm_OTU_with_counts.fa
+OUTHITS=../OTU_clustering/FLASH_combined_unique_seqs-min_2_reads--FILTER_OTU_MIN_100_READS-Swarm_OTU_with_counts.megablast
 
 REF=/home/cwarden/Ref/BLAST/nt
 
 #/opt/ncbi-blast-2.11.0+/bin/makembindex -input $REF -iformat blastdb
 
-/opt/ncbi-blast-2.11.0+/bin/blastn -task megablast -use_index true -db $REF -query $INFA -out $OUTHITS -evalue 1e-05 -max_target_seqs 1 -num_threads 1 -outfmt "6 qseqid sseqid evalue bitscore length pident frames staxids sskingdoms sscinames scomnames sblastnames stitle qseq qstart qend"
+/opt/ncbi-blast-2.11.0+/bin/blastn -num_threads 4 -task megablast -use_index true -db $REF -query $INFA -out $OUTHITS -evalue 1e-05 -max_target_seqs 1 -outfmt "6 qseqid sseqid evalue bitscore length pident frames staxids sskingdoms sscinames scomnames sblastnames stitle qseq qstart qend"
