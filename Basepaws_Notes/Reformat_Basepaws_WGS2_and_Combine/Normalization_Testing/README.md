f559e8eb0eadd5e6d53b1ad46b625b6dff61399f
Charles Warden <cwarden45@gmail.com>
2022-12-17 12:35:42 -0800

    Add files via upload

---
+++ Basepaws_Notes/Reformat_Basepaws_WGS2_and_Combine/Normalization_Testing/README.md
@@ -0,0 +1,54 @@
+The [Basepaws preprint](https://www.biorxiv.org/content/10.1101/2021.04.23.441192v1) mentions applying a "Centered Log-Ratio (CLR) transformation" to Braken classifications.
+
+While I think visual inspection of evenness of coverage can be useful, I do not believe that the separate [Bowtie2/BWA-MEM analysis with 11 representative sequences](https://github.com/cwarden45/Bastu_Cat_Genome/tree/master/Basepaws_Notes/Reformat_Basepaws_WGS2_and_Combine/Additional_Alignments) is appropriate to use for testing alterative normalizations (and essenitally needs to use an alternative set of total counts for normalization and percentage of non-host calculation).
+
+However, I am going to use those genera as a guide for comparison for the Kraken2/Bracken assignments that I made for my samples (and add back in the uBiome 16S samples):
+
+***i)*** Primarioly for human samples, create dot plot to see effect on *Streptococcus* abundance (but plot both human and cat)
+
+***ii)*** For human and cat samples, *heatmap with 11 genera* used for alternative alignments.
+
+## Direct Percent Non-Host Quantification
+
+Plots below are created using `test1-direct_quantification.R`.
+
+![Streptococcus Dot Plot](test1-n8_Oral_Kraken2_Braken-direct_quantification-Staphylococcus.png "Streptococcus Dot Plot")
+
+![11 Genera Heatmap](test1-n8_Oral_Kraken2_Braken-direct_quantification-heatmap_n11.png "11 Genera Heatmap")
+
+## ALDEx2 CLR Normalization (mean, default of `denom="all"`, use median for MonteCarlo simulations)
+
+Plots below are created using `test2-ALDEx2_CLR.R`, with [ALDEx2](https://bioconductor.org/packages/release/bioc/html/ALDEx2.html) dependency.
+
+![Streptococcus Dot Plot](test2-n8_Oral_Kraken2_Braken-ALDEx2_CLR-Staphylococcus.png "Streptococcus Dot Plot")
+
+![11 Genera Heatmap](test2-n8_Oral_Kraken2_Braken-ALDEx2_CLR-heatmap_n11.png "11 Genera Heatmap")
+
+## ALDEx2 Median Normalization (log ratio for median)
+
+The original goal was to use ALDEx2 with `denom="median"`.  However, I recieved an error message that *"denom: 'median' unrecognized. Using all features."*.  So, I added 1 to all counts (to avoid taking a log of zero) and I manually normalized to the median count (among the **original non-zero values**), without the MonteCarlo instances.
+
+So, plots below are created using `test3-log_median.R`.
+
+![Streptococcus Dot Plot](test3-n8_Oral_Kraken2_Braken-log2_median-Staphylococcus.png "Streptococcus Dot Plot")
+
+![11 Genera Heatmap](test3-n8_Oral_Kraken2_Braken-log2_median-heatmap_n11.png "11 Genera Heatmap")
+
+## ALDEx2 IQLR Normalization (Inter-Quartile Log-Ratio,`denom="iqlr"`, described in [Quinn et al. 2018](https://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-018-2261-8), use median for MonteCarlo simulations)
+
+**NOTE:** When using **R v3.6.3**, I received the error message:
+
+```
+Error in apply(reads.clr[these.rows, ], 2, function(x) { :
+  dim(X) must have a positive length
+Calls: aldex.clr ... aldex.clr.function -> aldex.set.mode -> iqlr.features -> apply
+
+```
+
+When I used to use my Windows PC (instead of the Ubuntu server where all other genomic dependenies are installed) with **R v4.2.1**, I receive a similar error message.
+
+Plots below are created using `test4-ALDEx2_IQLR.R`, with [ALDEx2](https://bioconductor.org/packages/release/bioc/html/ALDEx2.html) dependency.
+
+![Streptococcus Dot Plot](test4-n8_Oral_Kraken2_Braken-ALDEx2_IQLR-Staphylococcus.png "Streptococcus Dot Plot")
+
+![11 Genera Heatmap](test4-n8_Oral_Kraken2_Braken-ALDEx2_IQLR-heatmap_n11.png "11 Genera Heatmap")
---
+++ Basepaws_Notes/Reformat_Basepaws_WGS2_and_Combine/Normalization_Testing/test4-ALDEx2_IQLR.R
@@ -0,0 +1,85 @@
+#setwd("E:\\Bastu_Genome\\WGS2\\Reformat_Basepaws_WGS2_and_Combine\\Normalization_Testing")
+
+input_reference_counts = "../n29_FILTERED_Braken_genera-counts.txt"
+filtered_genera_list="../Additional_Alignments/Bacteria11.bed"
+Samples  =   c("Basepaws_WGS1","Basepaws_WGS2","Bristle1","Nebula_lcWGS","SequencingDotCom_WGS","uBiome_Oral2014","uBiome_Oral2019","Veritas_WGS"             )
+Species  =   c("Cat"          ,"Cat"          ,"Human"   ,"Human"       ,"Human"               ,"Human"          ,"Human"          ,"Human (Negative Control)")
+SampleSite = c("Oral"         ,"Oral"         ,"Oral"    ,"Oral"        ,"Oral"                ,"Oral"           ,"Oral"           ,"Oral"                    )
+output.percent_quantified = "test4-n8_Oral_Kraken2_Braken-ALDEx2_IQLR.txt"
+dot_plot.output = "test4-n8_Oral_Kraken2_Braken-ALDEx2_IQLR-Staphylococcus.png"
+heatmap.output = "test4-n8_Oral_Kraken2_Braken-ALDEx2_IQLR-heatmap_n11.png"
+
+non_host.count_table = read.table(input_reference_counts, head=T, sep="\t")
+oral_table = non_host.count_table[,match(Samples,names(non_host.count_table))]
+rownames(oral_table)=non_host.count_table$Genus
+
+library(ALDEx2)
+
+ALDEx2_object = aldex.clr(oral_table, paste(Species,SampleSite,sep=""), denom="iqlr")
+ALDEx2_MonteCarlo = getMonteCarloInstances(ALDEx2_object)
+
+norm_table=matrix(ncol=ncol(oral_table), nrow=nrow(oral_table))
+colnames(norm_table)=names(ALDEx2_MonteCarlo)
+rownames(norm_table)=rownames(oral_table)
+for (i in 1:ncol(norm_table)){
+	temp_norm= apply(data.frame(ALDEx2_MonteCarlo[i]),1,median)
+	norm_table[,i] = temp_norm[match(rownames(norm_table), names(temp_norm))]
+}#end for (i in 1:ncol(norm_table))
+norm_table=norm_table[,match(Samples,colnames(norm_table))]
+
+write.table(data.frame(Genus=rownames(norm_table), norm_table), output.percent_quantified, quote=F, sep="\t", row.names=F)
+
+filtered_genera_bed = read.table(filtered_genera_list, head=F, sep="\t")
+filtered_genera = as.character(filtered_genera_bed$V1)
+filtered_genera = gsub("_.*$","",filtered_genera)
+
+speciesCol=rep("black",length(Species))
+speciesCol[Species == "Cat"]=rainbow(4)[1]
+speciesCol[Species == "Human"]=rainbow(4)[2]
+speciesCol[Species == "Human (Negative Control)"]="black"
+
+siteCol=rep("black",length(Species))
+siteCol[SampleSite == "Oral"]=rainbow(4)[3]
+siteCol[SampleSite == "Fecal"]=rainbow(4)[4]
+
+#based on https://github.com/cwarden45/RNAseq_templates/blob/master/TopHat_Workflow/gene_plots.R
+png(dot_plot.output, type="cairo")
+par(mar=c(15,5,5,2))
+plot(as.numeric(as.factor(Samples)), as.numeric(norm_table[rownames(norm_table)=="Streptococcus",]),
+		xaxt="n", xlim=c(0,length(Samples)+1),
+		xlab="", ylab="ALDEX Median CLR-IQLR (Streptococcus)", pch=16, col=speciesCol,
+		cex=1.2, main="")
+legend("top",c("Cat","Human","Human (Negative Control)"),col=c(rainbow(4)[1],rainbow(4)[2],"black"),
+		cex=1.3, pch=16, inset=-0.33, xpd=T, ncol=2)
+mtext(Samples, side=1, at =1:length(Samples), las=2, line=2, font=2)
+dev.off()
+
+#based largely on https://github.com/cwarden45/DTC_Scripts/blob/master/Psomagen_Viome/mothur_analysis/mothur_genera_clustering.R
+#uses R v3.6.3 and gplots v3.1.1
+library(gplots)
+
+heatmap.abundance = norm_table[match(filtered_genera, rownames(norm_table)),]
+
+source("../heatmap.3.R")
+
+column_annotation = as.matrix(data.frame(Species = speciesCol,SampleSite = siteCol))
+colnames(column_annotation)=c("Species","SampleSite")
+
+#copied from https://github.com/cwarden45/RNAseq_templates/blob/master/TopHat_Workflow/DEG_goseq.R
+cor.dist = function(mat){
+	cor.mat = cor(as.matrix(t(mat)))
+	dis.mat = 1 - cor.mat
+	return(as.dist(dis.mat))	
+}#end def cor.dist
+
+png(heatmap.output, type="cairo")
+#would use Euclidean distance because some samples may all be 0 --> skip because this clustering doesn't look good
+heatmap.3(heatmap.abundance,   distfun = dist, hclustfun = hclust,
+			dendrogram="none", Rowv =F, Colv = F,
+			col=colorpanel(33, low="black", mid="pink", high="red"), density.info="none", key=TRUE,
+			ColSideColors=column_annotation, ColSideColorsSize=2, cexRow=1.8, cexCol=1.6,
+			trace="none", margins = c(18,15))
+legend("topright", pch=15, ncol=1, pt.cex=0.75, cex=0.9,
+		legend=c("Fecal","Oral","","Human","Cat"),
+		col=c(rainbow(4)[4],rainbow(4)[3],"white",rainbow(4)[2],rainbow(4)[1]))
+dev.off()
